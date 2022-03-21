eebca8e349bc12ac1d4d754f3bbda807206237bf
Bram Verspeek <bram.verspeek@gmail.com>
2022-03-21 23:16:24 +0100

    Sounds | Lighting | Two levels

--- Project Boost/Assets/Scripts/CollisionHandler.cs
+++ Project Boost/Assets/Scripts/CollisionHandler.cs
@@ -4,38 +4,94 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private float delayOnCrash = 1f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] float crashVolume = 0.5f;
+    [SerializeField] ParticleSystem crashParticle;
+    [SerializeField] AudioClip finishSound;
+    [SerializeField] float finishVolume = 0.5f;
+    [SerializeField] ParticleSystem finishParticle;
+
+    private AudioSource audioSource;
+
+    bool isTransitioning;
+    bool collisionsOn = true;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsOn = !collisionsOn;
+            Debug.Log($"CollisionsOn: {collisionsOn}");
+        }
+        if (Input.GetKeyDown(KeyCode.L)) { StartNextLevelSequence(); }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        switch (collision.gameObject.tag)
+        if (isTransitioning || !collisionsOn)
         {
-            case "Friendly":
-                Debug.Log("We hit a friendly");
-                break;
-            case "Finish":
-                StartNextLevelSequence();
-                break;
-            default:
-                StartCrashSequence();
-                break;
+            return;
+        }
+
+        if (collision.gameObject.GetInstanceID() != this.gameObject.GetInstanceID())
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Player":
+                case "Friendly":
+                    Debug.Log("We hit a friendly");
+                    break;
+                case "Finish":
+                    StartNextLevelSequence();
+                    break;
+                default:
+                    StartCrashSequence();
+                    break;
+            }
         }
     }
 
     private void StartNextLevelSequence()
     {
+        isTransitioning = true;
         StopRocket();
+        PlaySound(finishSound, finishVolume);
+        PlayParticle(finishParticle);
         Invoke(nameof(GoToNextLevel), delayOnCrash);
     }
 
     void StartCrashSequence()
     {
-        StopRocket();
-        Invoke(nameof(ReloadLevel), delayOnCrash);
+        if (!isTransitioning)
+        {
+            isTransitioning = true;
+            StopRocket();
+            PlaySound(crashSound, crashVolume);
+            PlayParticle(crashParticle);
+            Invoke(nameof(ReloadLevel), delayOnCrash);
+        }
+    }
+
+    private void PlaySound(AudioClip audioClip, float volume)
+    {
+        audioSource.PlayOneShot(audioClip, volume);
+    }
+
+    private void PlayParticle(ParticleSystem particleSystem)
+    {
+        particleSystem.Play();
     }
 
     private void StopRocket()
     {
-        GetComponent<Movement>().enabled = false;
+        var movement = GetComponent<Movement>();
+        movement.StopRocket();
+        movement.enabled = false;
         var audioSource = GetComponent<AudioSource>();
         if (audioSource.isPlaying)
         {
--- Project Boost/Assets/Scripts/Movement.cs
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -7,38 +7,41 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float turningThrust = 90f;
-    
-    
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem engineParticles;
+    [SerializeField] ParticleSystem leftEngine;
+    [SerializeField] ParticleSystem rightEngine;
+
     private Rigidbody rb;
     private AudioSource audioSource;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ProcessThrust();
         ProcessRotation();
     }
 
+    public void StopRocket()
+    {
+        OnStopRocketThrust();
+        OnStopRocketTurn();
+    }
+
     private void ProcessThrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            rb.AddRelativeForce(Time.deltaTime * mainThrust * Vector3.up);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            OnRocketThrust();
         }
-        else
+        else if(Input.GetKeyUp(KeyCode.Space))
         {
-            audioSource.Stop();
+            OnStopRocketThrust();
         }
     }
 
@@ -46,12 +49,60 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            RotateObject(turningThrust);
+            OnRocketTurn(rightEngine, turningThrust);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            RotateObject(-turningThrust);
+            OnRocketTurn(leftEngine, -turningThrust);
         }
+        else if (
+            Input.GetKeyUp(KeyCode.A) || 
+            Input.GetKeyUp(KeyCode.LeftArrow)  || 
+            Input.GetKeyUp(KeyCode.RightArrow) || 
+            Input.GetKeyUp(KeyCode.D))
+        {
+            OnStopRocketTurn();
+        }
+    }
+
+
+    private void OnRocketThrust()
+    {
+        rb.AddRelativeForce(Time.deltaTime * mainThrust * Vector3.up);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine, 0.145f);
+        }
+        if (!engineParticles.isPlaying)
+        {
+            engineParticles.Play();
+        }
+    }
+
+    private void OnRocketTurn(ParticleSystem engineParticle, float turningThrust)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine, 0.07f);
+        }
+        if (!engineParticle.isPlaying)
+        {
+            engineParticle.Play();
+        }
+        RotateObject(turningThrust);
+    }
+
+    private void OnStopRocketThrust()
+    {
+        audioSource.Stop();
+        engineParticles.Stop();
+    }
+
+    private void OnStopRocketTurn()
+    {
+        audioSource.Stop();
+        rightEngine.Stop();
+        leftEngine.Stop();
     }
 
     private void RotateObject(float rotationPerSecond)
